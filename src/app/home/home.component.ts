8d83f92c27f380277acfb63984da4537a8f79aa6
Jack Allard <jackallard17@gmail.com>
2022-03-01 00:14:57 -0800

    refactored homepage

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent{
+}
